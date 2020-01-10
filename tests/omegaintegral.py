50360861c7c11305a9a4231fa163a958b713c49a
Tommy Hentschel <th584@cornell.edu>
2020-01-10 16:14:51 -0500

    More tests

--- tests/omegaintegral.py
+++ tests/omegaintegral.py
@@ -17,12 +17,13 @@ import dielectricfunction.MerminDielectric as MD
 
 # Define some fixed parameters
 
-# Need to input these manually from an external source - these three are all 
-# related.
+# Need to input these manually from an external source - the first three are 
+# all related.
 T    = 6     # eV
 ne   = 1e23  # e/cc
 muau = 0.126 # au
 
+nu = 1
 # Convert to a.u. if necessary
 ev2au = 1/27.2114 #a.u./eV
 a0 = 0.529*10**-8 # Bohr radius, cm
@@ -33,18 +34,18 @@ neau  = ne * 1/cm2au**3
 # Important constants
 kF = np.sqrt((3 * np.pi**2 * neau)**(2/3))
 wp = np.sqrt(4 * np.pi * neau)
-sumrule = np.pi/2 * wp
+sumrule = np.pi/2 * wp**2
 
 
 ####### Step 1: Take a peak #######
 
 # Make the data
 k = 0.5 # a.u.
-w = np.linspace(0, 4*wp, 200)
-elf = np.asarray([MD.ELF(k, x, Tau, muau, 0) for x in w])
+w = np.linspace(0, 12*wp, 200)
+elf = np.asarray([MD.ELF(k, x, Tau, muau, nu) for x in w])
 
 # Let's plot!
-plt.plot(w/wp, elf, label='k = {} au'.format(k))
+plt.plot(w/wp, elf, label='k = {} au'.format(k), c='C1')
 plt.title(r'Al z* = 3; $T$ = {} eV, $n_e$ = {} e/cc'.format(T, ne))
 plt.xlabel(r'$\omega/\omega_p$')
 plt.ylabel('ELF')
@@ -61,22 +62,22 @@ We will just eye-ball the upper limit of the integral using the graphs from
 step 1.
 '''
 
-k = 0.5
+k = k
 
 # Define the integrand
-omegaintegrand = lambda w, y : w * MD.ELF(k, w, Tau, muau, 0)
+omegaintegrand = lambda w, y : w * MD.ELF(k, w, Tau, muau, nu)
 
 # Set the initial value
 y0 = [0]
 # Set the integration limits
-wlim = (0, 4*wp)
+wlim = (0, 12*wp)
 
 # Set the tolerance
-tol = 1.49012e-8
+tol = 1.49012e-6
 # Solve the corresponding ODE
 omegaintegral = solve_ivp(omegaintegrand, wlim, y0, #method='LSODA',
                           rtol=tol, atol=tol)
 
 print("omega integral = {} ; sum rule = {}".format(omegaintegral.y[0][-1],
                                                    sumrule))
-plt.scatter(omegaintegral.t, np.zeros(len(omegaintegral.t)))
+plt.scatter(omegaintegral.t/wp, np.zeros(len(omegaintegral.t)), marker='+')
