ba0eec127776c453188ff6b6d72ee5d0b7b8e7cd
Tommy Hentschel <th584@cornell.edu>
2020-05-03 22:05:00 -0400

    Maybe a running stopping power test

--- tests/omegaintegral_mermin.py
+++ tests/omegaintegral_mermin.py
@@ -10,6 +10,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy import interpolate
 import scipy.integrate as integrate
+from scipy import optimize
 
 from dielectricfunction_symln.Mermin import MerminDielectric as MD
 
@@ -86,31 +87,55 @@ def omegaint(v, k, nu, T, mu):
     
     return integrate.trapz(integrand, w)
 
-v = [0.1, 1, 5, 10]
-k = np.linspace(5e-2, 5, 2)
-fig, ax = plt.subplots()
-for y in v:
-    kint = [omegaint(y, x, nu, Tau, muau) for x in k]
-    p = ax.plot(k, kint, label='v={}'.format(y))
-    ax.axvline(2*y, linestyle='-.', c=p[-1].get_color())
+### Look at the k-integrand ###
+# v = [0.1, 1, 5, 10]
+# k = np.linspace(5e-2, 5, 2)
+# fig, ax = plt.subplots()
+# for y in v:
+#     kint = [omegaint(y, x, nu, Tau, muau) for x in k]
+#     p = ax.plot(k, kint, label='v={}'.format(y))
+#     ax.axvline(2*y, linestyle='-.', c=p[-1].get_color())
 
-plt.xlabel(r'$k (1/a_0)$')
-plt.title('k-integrand for Al at T=6 eV')
-plt.legend()
-plt.show()
+# ax.set_xlabel(r'$k ~ (1/a_0)$')
+# ax.set_title('k-integrand for Al at T=6 eV')
+# ax.legend()
 
-# def momint(v, nu, T, mu):
+# plt.show()
+
+def momint(v, nu, T, mu, k0):
     
-#     # Define the integrand
-#     #momintegrand = lambda k: omegaint(v, k, nu, T, mu)
-#     k = np.linspace(5e-2, 2*v)
-#     integrand = [omegaint(v, x, nu, Tau, muau) for x in k]
+    # Define the integrand
+    #momintegrand = lambda k: omegaint(v, k, nu, T, mu)
+    k = np.linspace(k0, 2*v, 100)
+    integrand = [omegaint(v, x, nu, Tau, muau) for x in k]
     
-#     return integrate.trapz(integrand, k)
-    
-    
-# v = np.linspace(0, 10, 10)
-# S = [momint(x, nu, Tau, muau) for x in v]
-# plt.scatter(v, S)
+    return integrate.trapz(integrand, k)
+
+def drude_ELF(w, nu, wp):
+    drude_eps = 1 - wp**2 / (w**2 + 1j * w * nu(w))
+    return drude_eps.imag / (drude_eps.real**2 + drude_eps.imag**2)
+
+
+def error(v, nu, wmax, wp, k0):
+    # find maximum of the drude ELF
+    return v * k0 * wmax * drude_ELF(wmax, nu, wp)
+
+v = np.linspace(1e-3, 10, 2)
+S = [2 / np.pi / x**2 * momint(x, nu, Tau, muau, 5e-2) for x in v]
+S = np.asarray(S)
+# Save data just in case something breaks
+np.save('stopping_data_tmp', S)
+
+wmax_drude = 0.588
+S_error = [2 / np.pi / x**2 * error(x, nu, wmax_drude, wpau, 5e-2) for x in v]
+S_error = np.asarray(S_error)
+#prefactor = 2 / np.pi / v**2
+plt.plot(v, S)
+plt.fill_between(v, S, S + S_error, alpha=0.2)
+
+plt.xlabel('v (au)')
+plt.ylabel('Stopping Power (au)')
+plt.title('Stopping power for solid Al at 6 ev')
+
 
 
