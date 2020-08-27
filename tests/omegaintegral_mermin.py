89427f8c81ea272c62c24fdc7616f3dd912b867e
Tommy <th584@cornell.edu>
2020-08-26 23:00:46 -0400

    implement sequential integration scheme

--- tests/omegaintegral_mermin.py
+++ tests/omegaintegral_mermin.py
@@ -14,12 +14,20 @@ from scipy.integrate import solve_ivp
 from scipy import optimize
 import time
 
-from dielectricfunction_symln.Mermin import MerminDielectric as MD
+from Mermin import MerminDielectric as MD
+from xMermin import xMermin as xmd
+
+# for PIMC UEG data
+from keras import losses
+from keras.models import Sequential
+from keras import regularizers
+from keras.layers import Dense, Dropout
+from keras.layers.advanced_activations import LeakyReLU
 
 ######## Parameters ###############
 a0 = 0.529*10**-8 # Bohr radius, cm
-TeV = 6 # Temperature, eV
-ne_cgs = 1.8*10**23 # electron density, cm^-3
+TeV = 10 # Temperature, eV
+ne_cgs = 6*10**23 # electron density, cm^-3
 neau = ne_cgs * a0**3 # electron density, au
 EFau =  0.5*(3*np.pi**2*neau)**(2/3)# Fermi energy, au
 kFau = np.sqrt(2*EFau) # Fermi momentum/wavevector, au
@@ -30,17 +38,16 @@ sumrule = np.pi/2 * wpau**2
 
 # Needed to assume some mu. For this, we are assuming the material and then
 # using a spreadsheet that Stephanie prepared to get mu
-muau = 0.305 # mu for aluminum, with ne_cgs=1.8*10**23, T=1ev, Z*=3; au
+muau = 0.783 # mu for Hydrogen at 10 eV, 10g/cc
 
 #########################################
 
 ###### Collision frequencies data ########
-filename = "tests/Al_6_eV_vw.txt"
-wdata, RenuT, RenuB, ImnuT, ImnuB = np.loadtxt(filename, skiprows = 1, 
-                                               unpack=True)
+filename = "tests/H_10gpcc_10_eV_vw.txt"
+wdata, RenuT, ImnuT = np.loadtxt(filename, unpack=True)
 
 wmin = min(wdata)
-nu = 1j*ImnuT; nu += RenuT
+#nu = 1j*ImnuT; nu += RenuT
 
 # Parametrize
 f_renu = interpolate.interp1d(wdata, RenuT)
@@ -49,9 +56,57 @@ f_imnu = interpolate.interp1d(wdata, ImnuT)
 nu = lambda w: f_renu(w) + 1j*f_imnu(w)
 #nu = lambda w: neau * RenuT[0]/(RenuT[0]**2 + w**2)
 
+# def nu(w): 
+#     return 1e-2 # Don't have collisions !
+        
+##### Use a simple expression for collision frequencies #####
+# def nu(w):
+#     return RenuT[0] / (1 + w**2)
 ##### Omega integral #####
 # w = np.linspace(wmin, 10, 200)
 # k = 1e-1
+######################################################
+
+#########################################
+
+"""
+Obtain static local field correction from PIMC data of uniform electron gas.
+"""
+# Define LFC
+LR = LeakyReLU()
+LR.__name__ = 'relu'
+
+# Define the Keras model
+
+N_LAYER = 40
+W_LAYER = 64
+
+model = Sequential()
+model.add(Dense(W_LAYER, input_dim=3, activation=LR))
+
+REGULARIZATION_RATE = 0.0000006
+
+for i in range(N_LAYER-1):
+	model.add( Dense( W_LAYER, activation=LR, 
+                  kernel_regularizer=regularizers.l2( REGULARIZATION_RATE)))
+
+model.add(Dense(1, activation='linear'))
+
+# Load the trained weights from hdf5 file
+model.load_weights('../dielectricfunction/LFC/tests/LFC.h5')
+
+# Define simple wrapper function (x=q/q_F):
+
+def GPIMC(q, ne, T):
+    rs = (3/4/np.pi/ne)**(1./3)
+    kF = (9.0*np.pi/4.0)**(1.0/3.0)/rs
+    EF = 0.5*(3*np.pi**2*ne)**(2./3)
+    x = q/kF
+    y = T/EF
+    result = model.predict( np.array( [[x,rs,y]] ) )
+    return result[0][0]
+
+########################################################
 
 # # Define the integrand
 # omegaintegrand = lambda x, y : x * MD.ELF(k, x, nu(x), Tau, muau)
@@ -91,32 +146,35 @@ def omegaint(k, v, nu, T, mu):
     
     return integrate.trapz(integrand, w)
 
-def omegaint_adapt(k, v, nu, T, mu):
+def omegaint_adapt(k, vlow, vhigh, nu, T, mu, G):
     
-    omegaintegrand = lambda x, y : x / k * MD.ELF(k, x, nu(x), T, mu) 
+    omegaintegrand = lambda x, y : x * xmd.ELF(k, x, nu(x), T, mu, 0)
+    # G(k, neau, Tau)) 
     
-    if k*v < wmin :
+    if k*vlow < wmin :
         return 0
-    if k*v > max(wdata):
+    if k*vhigh > max(wdata):
         return 0
-    
-    return solve_ivp(omegaintegrand, (wmin, k*v), [0]).y[0][-1]
+    I = solve_ivp(omegaintegrand, (k*vlow, k*vhigh), [0], rtol=1e-6, 
+                  atol=1e-6)
+
+    return I.y[0][-1]
 
 ### Look at the k-integrand ###
-v = [5, 7, 10, 11]
-k = np.linspace(0.1,0.3, 100)
-fig, ax = plt.subplots()
-for y in v:
-    kint = [omegaint_adapt(y, x, nu, Tau, muau) for x in k]
-    p = ax.plot(k, kint, label='v={}'.format(y))
-    ax.axvline(2*y, linestyle='-.', c=p[-1].get_color())
+# v = [7]
+# k = np.linspace(1e-3, 0.5, 100)5B
+# fig, ax = plt.subplots()
+# for y in v:
+#     kint = [omegaint_adapt(x, y, nu, Tau, muau) for x in k]
+#     p = ax.plot(k, kint, label='v={}'.format(y))
+#     #ax.axvline(2*y, linestyle='-.', c=p[-1].get_color())
 
-ax.set_xlabel(r'$k (1/a_0)$')
-ax.set_title('k-integrand for Al at T=6 eV')
-ax.legend()
+# ax.set_xlabel(r'$k (1/a_0)$')
+# ax.set_title('k-integrand for Al at T=6 eV')
+# ax.legend()
 
-plt.savefig('kintegrand1')
-plt.show()
+# #plt.savefig('kintegrand2')
+# plt.show()
 
 
 def momint(v, nu, T, mu, k0):
@@ -124,34 +182,56 @@ def momint(v, nu, T, mu, k0):
     # Define the integrand
     #momintegrand = lambda k: omegaint(v, k, nu, T, mu)
     k = np.linspace(k0, 2*v, 100)
-    integrand = [omegaint(x, v, nu, Tau, muau) for x in k]
+    integrand = [1/ x * omegaint(x, v, nu, Tau, muau) for x in k]
     
     return integrate.trapz(integrand, k)
 
-def momint_adapt(v, nu, T, mu, k0):
-    kintegrand = lambda k, y : omegaint_adapt(k, v, nu, T, mu)
+def momint_adapt(vlow, vhigh, nu, T, mu, G, k0):
+    kintegrand = lambda k, y : 1/k * omegaint_adapt(k, vlow, vhigh, nu, T, mu, G)
+    sol = solve_ivp(kintegrand, (k0, 2*vhigh), [0])
+    return sol.y[0][-1]
+
+def drude_ELF(w, nu, wp):
+    drude_eps = 1 - wp**2 / (w**2 + 1j * w * nu(w))
+    return drude_eps.imag / (drude_eps.real**2 + drude_eps.imag**2)
+
+
+def error1(v, nu, wmax, wp, k0):
     
-    return solve_ivp(kintegrand, (k0, 2*v), [0]).y[0][-1]
+    return v * k0 * wmax * drude_ELF(wmax, nu, wp)
 
-# def drude_ELF(w, nu, wp):
-#     drude_eps = 1 - wp**2 / (w**2 + 1j * w * nu(w))
-#     return drude_eps.imag / (drude_eps.real**2 + drude_eps.imag**2)
+def error2(v, nu, T, mu, k0):
+    return v**2 * k0**2 / 2 * MD.ELF(k0, k0*v, nu(k0*v), T, mu)
 
+def sequentialstopping(varr, nu, T, mu, G, k0, L0=0):
+    '''
+    varr: array-like
+        Array of velocities. Assumption is that the first velocity corresponds
+        zero stopping, but you can control this with L0.
+    '''
+    L = np.zeros(len(varr))
+    L[0] = L0
+    for i in range(1, len(v)):
+        L[i] = momint_adapt(v[i-1], v[i], nu, T, mu, G, k0)
 
-# def error1(v, nu, wmax, wp, k0):
-    
-#     return v * k0 * wmax * drude_ELF(wmax, nu, wp)
+    return np.cumsum(L)
 
-# def error2(v, nu, T, mu, k0):
-#     return v**2 * k0**2 / 2 * MD.ELF(k0, k0*v, nu(k0*v), T, mu)
+v = np.linspace(1e-3, 12, 100)
+v = np.append([0], v)
+k0 = 5e-2
+s = time.time()
+#S = [momint_adapt(x, nu, Tau, muau, GPIMC, 5e-2) for x in v]
+L = sequentialstopping(v, nu, Tau, muau, GPIMC, k0)
+runtime = 'run time = {} s\n'.format(time.time() - s)
+parameters = 'Te = {} [eV]\nne = {:e} [1/cc]\nmu = {} [au]\n'.\
+             format(TeV, ne_cgs,muau)
+head = 'v[a.u.]    stopping number[a.u.]'
+L = np.asarray(L)
+# Save data just in case something breaks
+np.savetxt('stopdata_Mermin_hydrogen_1seq.txt', np.transpose([v, L]), 
+           header = runtime + parameters + head)
 
-# v = np.linspace(1e-3, 10, 100)
-# #S = [2 / np.pi / x**2 * momint(x, nu, Tau, muau, 5e-2) for x in v]
-# #S = np.asarray(S)
-# # Save data just in case something breaks
-# #np.save('stopping_data_tmp', S)
-
-# S = np.load('tests/stopping_data_tmp.npy') / kFau**2
+# # S = np.loadtxt('stopping_data_adapt_tmp.out') / kFau**2
 
 # wmax_drude = 0.588
 # S_error1 = [2 / np.pi / x**2 * error1(x, nu, wmax_drude, wpau, 5e-2) 
@@ -162,12 +242,13 @@ def momint_adapt(v, nu, T, mu, k0):
 # S_error2 = [2 / np.pi / x**2 * error2(x, nu, Tau, muau, 5e-2) 
 #             for x in v]
 # S_error2 = np.asarray(S_error2) / kFau**2
-# plt.plot(v, S)
-# plt.fill_between(v[1:], S[1:], S[1:] + S_error1[1:], color='C2', alpha=0.2)
+
+# plt.figure()
+# #plt.fill_between(v[1:], S[1:], S[1:] + S_error1[1:], color='C2', alpha=0.2)
 # plt.fill_between(v, S, S + S_error2, color = 'C1',  alpha=0.2)
 # plt.xlabel(r'$v$ (au)')
 # plt.ylabel(r'$S(v) / Z^2$')
 # plt.title('Stopping power for solid Al at 6 ev')
 
-# plt.savefig('stoppingpower')
+# plt.savefig('stoppingpower_drudenu2')
 
